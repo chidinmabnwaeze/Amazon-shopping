b65ebdabd823bc4ef9f1984b5e41b2eb93db3b7e
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-10-25 06:19:52 +0100

    made the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -79,6 +79,14 @@ document.querySelectorAll(".js-addToCart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+
+    let cartQuantity = 0;
+
+    cart.forEach((item)=>{
+      cartQuantity += item.quantity
+    })
+    document.querySelector(".js-cartQuantity").innerHTML = cartQuantity
+    
   });
 });
