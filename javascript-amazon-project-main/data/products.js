bb9ab74dbaad5f63c478a9601b16a55106c88112
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-11-06 19:14:01 +0100

    convert products into classes

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../scripts/utils/money.js";
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -6,9 +7,34 @@ export function getProduct(productId) {
       matchingProduct = product;
     }
   });
-  
+
   return matchingProduct;
 }
+
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.priceCents = productDetails.priceCents;
+    this.rating = productDetails.rating;
+  }
+
+  getStarsUrl(){
+   return `images/ratings/rating-${this.rating.stars * 10}.png`  
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`
+  } 
+}
+
 
 export const products = [
   {
@@ -481,4 +507,7 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails)=>{
+return new Product(productDetails)
+});
+
